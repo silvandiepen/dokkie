d9d69b59755600a89042227fcfca898aa0793a4e
Sil van Diepen <me@silvandiepen.com>
2020-06-15 17:47:29 +0200

    remove log

--- src/steps/page.ts
+++ src/steps/page.ts
@@ -109,8 +109,6 @@ export const createFiles = async (settings: ISettings): Promise<void> => {
 		scripts: settings.scripts ? settings.scripts : null,
 	};
 
-	console.log(getOnce.favicon);
-
 	log.BLOCK_MID("Creating pages");
 	await asyncForEach(settings.files, async (file: IFile) => {
 		// THe file is newer than today, so don't build it (yet).
